da8425e0f90ff9cfca3f2b6cf9e0e338f6cf5348
Toby Clemson <tobyclemson@gmail.com>
2017-08-20 19:13:16 +0100

    [TECH] Upgrade to latest version of infrablocks modules.
    Authors: @tobyclemson

--- infra/secrets_bucket/bucket.tf
+++ infra/secrets_bucket/bucket.tf
@@ -1,5 +1,5 @@
 module "encrypted_bucket" {
-  source = "github.com/infrablocks/terraform-aws-encrypted-bucket?ref=0.1.3//src"
+  source = "github.com/infrablocks/terraform-aws-encrypted-bucket?ref=0.1.2//src"
 
   region = "${var.region}"
   bucket_name = "${var.bucket_name}"
