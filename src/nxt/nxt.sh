08f5d7481588c46256e118c1eec83985e499d7bb
Toby Clemson <tobyclemson@gmail.com>
2017-09-03 16:05:13 +0100

    [TECH] Allow download of blockchain archive when supplied in environment variable.
    Authors: @tobyclemson

--- src/nxt/nxt.sh
+++ src/nxt/nxt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 # Ensure AWS_REGION environment variable is present
 if [ -z "$AWS_REGION" ]; then
   echo >&2 'Error: missing AWS_REGION environment variable.'
@@ -12,17 +15,27 @@ if [ -z "$AWS_S3_CONFIGURATION_OBJECT" ]; then
   exit 1
 fi
 
+# Move to NXT directory
+cd /opt/nxt
+
 # Source default env file
-eval $(cat /opt/nxt/conf/nxt-default.env | sed 's/^/export /')
+eval $(cat conf/nxt-default.env | sed 's/^/export /')
 
 # Fetch and source overrides env file
 eval $(aws s3 cp --sse AES256 --region ${AWS_REGION} \
     ${AWS_S3_CONFIGURATION_OBJECT} - | sed 's/^/export /')
 
 # Render properties template
-cat /opt/nxt/conf/nxt.properties.template \
-    | envsubst > /opt/nxt/conf/nxt.properties
+cat conf/nxt.properties.template \
+    | envsubst > conf/nxt.properties
+
+# Fetch initial database archive if specified
+if [ -n "$NXT_INITIAL_BLOCKCHAIN_ARCHIVE_URL" ]; then
+    mkdir tmp
+    curl -L "$NXT_INITIAL_BLOCKCHAIN_ARCHIVE_URL" -o tmp/blockchain_archive.zip
+    unzip tmp/blockchain_archive.zip -d nxt_db
+    rm -rf tmp
+fi
 
 # Start NXT
-cd /opt/nxt
 ./run.sh
